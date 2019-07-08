e99733a918c7fb9fe6eda0b29a3a196d1d69940c
DanTimmermann <52572905+DanTimmermann@users.noreply.github.com>
2019-07-08 12:18:01 -0400

    Woo

---
+++ data/About_7.js
@@ -0,0 +1,8 @@
+var json_About_7 = {
+"type": "FeatureCollection",
+"name": "About_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Welcome": "About", "About": "This map was made using the current rosters for each team on Wikipedia as of July 1st, 2019. \n\nPlayers born in the same city were assigned random addresses within the city so as they did not stack on top of one another. \n\nKeep in mind the locations are players birthplaces not their hometown which in some cases is different. \n\nThe year acquired field is when the player was acquired by the current team.\n\nThere may be mistakes (Brad Marchand disapeared for a while), but I think the map is at least 99% accurate. If you find any or have any suggestions please email me at dantimmermann at gmail dot com. \n\nThis map was made in QGIS and converted for web using QGIS2Web." }, "geometry": { "type": "Point", "coordinates": [ -34.127426908502997, 45.311684406463286 ] } }
+]
+}
